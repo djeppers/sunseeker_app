bf2da919f965833f5f18a099f8b3e28258cd6cbd
Jeppe Lønow <jeppe@DESKTOP-TLDSFAC.localdomain>
2026-04-24 23:46:49 +0200

    added support for cloudpebble

--- src/pkjs/index.js
+++ src/pkjs/index.js
@@ -1,3 +1,22 @@
-const moddableProxy = require("@moddable/pebbleproxy");
-Pebble.addEventListener('ready', moddableProxy.readyReceived);
-Pebble.addEventListener('appmessage', moddableProxy.appMessageReceived);
+Pebble.addEventListener('ready', function() {
+    getLocation();
+});
+
+Pebble.addEventListener('appmessage', function() {
+    getLocation();
+});
+
+function getLocation() {
+    navigator.geolocation.getCurrentPosition(
+        function(pos) {
+            Pebble.sendAppMessage({
+                lat: Math.round(pos.coords.latitude * 10000),
+                lon: Math.round(pos.coords.longitude * 10000)
+            });
+        },
+        function(err) {
+            console.log('Location error: ' + err.message);
+        },
+        { enableHighAccuracy: true, maximumAge: 300000, timeout: 15000 }
+    );
+}
